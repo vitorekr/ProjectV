c65ff918a3c367c2324c345b9d6da0d1fa663fb1
vitorekr <56610229+vitorekr@users.noreply.github.com>
2023-12-28 11:42:47 -0300

    Add files via upload

---
+++ Data/TravelDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectV.Domain;
+
+namespace ProjectV.Data
+{
+    public class TravelDbContext : DbContext
+    {
+        public TravelDbContext(DbContextOptions<TravelDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Continent> Continent { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasOne(c => c.Capital)
+                .WithOne()
+                .HasForeignKey<Country>(c => c.CapitalId);
+
+            modelBuilder.Entity<Country>()
+                .HasOne(c => c.Continent)
+                .WithMany()
+                .HasForeignKey(c => c.ContinentId);
+
+            modelBuilder.Entity<City>()
+                .HasOne(c => c.Country)
+                .WithMany()
+                .HasForeignKey(c => c.CountryId);
+        }
+
+    }
+}
