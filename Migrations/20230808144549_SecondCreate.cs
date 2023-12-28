54d9c44bc8b0118dd9eedaeab3d808a6a8600756
vitorekr <56610229+vitorekr@users.noreply.github.com>
2023-12-28 11:43:18 -0300

    Add files via upload

---
+++ Migrations/20230808144549_SecondCreate.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectV.Migrations
+{
+    public partial class SecondCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capital",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "Continent",
+                table: "Countries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CapitalId",
+                table: "Countries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContinentId",
+                table: "Countries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Continent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Continent", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries",
+                column: "CapitalId",
+                unique: true,
+                filter: "[CapitalId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_ContinentId",
+                table: "Countries",
+                column: "ContinentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Cities_CapitalId",
+                table: "Countries",
+                column: "CapitalId",
+                principalTable: "Cities",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Continent_ContinentId",
+                table: "Countries",
+                column: "ContinentId",
+                principalTable: "Continent",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Cities_CapitalId",
+                table: "Countries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Continent_ContinentId",
+                table: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Continent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_ContinentId",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CapitalId",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "ContinentId",
+                table: "Countries");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Capital",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Continent",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20230809135848_ThirdCreate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectV.Migrations
+{
+    public partial class ThirdCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCapital",
+                table: "Cities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCapital",
+                table: "Cities");
+        }
+    }
+}
